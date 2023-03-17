7878dc1664ec12ea1cbda29b71e66034fd81d3d1
Y11RI <yuri_lino@hotmail.com>
2023-03-17 00:41:13 -0400

    feat: reajustando addAtom e show

--- GeneralizedLinkedList.c
+++ GeneralizedLinkedList.c
@@ -9,8 +9,14 @@ int addAtom(Node **list, int atom){
         no->type = 0;
         no->atomList.atom = atom;
         no->next = NULL;
-    
-        
+        if(list == NULL)
+         list = &no;
+        else{
+            Node *aux = &list->next;
+            while(aux->next != NULL)
+                aux= aux-> next;
+        aux->next = no;
+        }
     /*if(list -> list!=NULL){
         Node *aux = (Node*)malloc(sizeof(Node));
         aux = list->next;
@@ -19,16 +25,13 @@ int addAtom(Node **list, int atom){
             }
         aux->next = no;*/
     return 1;
-    
-
-
-
 }
 
-void show(Node *list){
-   Node *aux = (Node*)malloc(sizeof(Node));
-   aux->type = 1;
-   aux->atomList.list = list;
-    
-    return NULL;
-}
+/*void show(Node *list){
+   Node *aux2 = list->next;
+   aux2->type = 1;
+   while(aux2 != list){
+        printf(aux2->atomList.list);
+    aux2 = aux2->next; 
+   }
+}*/
