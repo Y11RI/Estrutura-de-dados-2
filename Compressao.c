a2fcc7afcff3d148dba56bbe141e8d07e5b4aa16
Y11RI <yuri_lino@hotmail.com>
2023-05-25 15:27:05 -0400

    1 passo de tartaruga pro projeto

--- Compressao.c
+++ Compressao.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct letras{
+char simbolo[256];
+int freq;
+
+}LETRAS;
+
+ int stlen(char * str){
+    int total =0;
+        while(str[total]!= '\0')
+                    total++;
+                return total;
+            }
 
 int main() {
-    char arquivo,a=0,e=0,i=0,o=0,u=0;
+    char arquivo[50],a=0,e=0,i=0,o=0,u=0;
     char c;
     char *dados;
+    char result[100];
+    
+
     printf("Insira o arquivo texto que será lido: ");
-    scanf("%s",&arquivo);
-    FILE *arq = fopen(&arquivo, "r");
+    scanf("%s",arquivo);
+    FILE *arq = fopen(arquivo, "r");
 
     if(arq == NULL){
         printf("arquivo de palavras vazio");
@@ -16,7 +33,11 @@ int main() {
         
          while(!feof(arq)){
             dados = fgets(result,100,arq);
-            do{
+            int tamanho = strlen(dados);
+            printf("%d\n",tamanho);
+            //printf("%d",total);
+            //printf("%s",dados);
+            /*do{
                 c = getc(dados);
                 switch(c){
                 case a:
@@ -35,7 +56,7 @@ int main() {
                     u++;
                     break;
                 }
-            }while(c!= "\n");
+            }while(c!= "\n");*/
 
          }
     }
